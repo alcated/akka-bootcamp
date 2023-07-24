100f01f673b0ec4dd3296fc511f7e71a91eba88a
Teddy Alcabelard <talcabelard@d-edge.com>
2023-07-24 23:38:25 +0200

    Remove the definition and call to PrintInstructions()

--- src/Unit-1/DoThis/Program.cs
+++ src/Unit-1/DoThis/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-﻿using Akka.Actor;
+using Akka.Actor;
 
 namespace WinTail
 {
@@ -13,8 +13,6 @@ namespace WinTail
             // initialize MyActorSystem
             MyActorSystem = ActorSystem.Create("MyActorSystem");
 
-            PrintInstructions();
-
             // time to make your first actors!
             var consoleWriterActor = MyActorSystem.ActorOf(Props.Create(()=> new ConsoleWriterActor() ));
             var consoleReaderActor = MyActorSystem.ActorOf(Props.Create(()=> new ConsoleReaderActor(consoleWriterActor) ));
@@ -27,21 +25,6 @@ namespace WinTail
             MyActorSystem.WhenTerminated.Wait();
         }
 
-        private static void PrintInstructions()
-        {
-            Console.WriteLine("Write whatever you want into the console!");
-            Console.Write("Some lines will appear as");
-            Console.ForegroundColor = ConsoleColor.DarkRed;
-            Console.Write(" red ");
-            Console.ResetColor();
-            Console.Write(" and others will appear as");
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.Write(" green! ");
-            Console.ResetColor();
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine("Type 'exit' to quit this application at any time.\n");
-        }
     }
     #endregion
 }
