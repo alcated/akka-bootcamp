a756626854ff500b961ce82accde182265b8d262
Teddy Alcabelard <talcabelard@d-edge.com>
2023-07-29 14:23:13 +0200

    Making IActorRefs using Props

--- src/Unit-1/DoThis/Program.cs
+++ src/Unit-1/DoThis/Program.cs
@@ -13,11 +13,16 @@ namespace WinTail
             // initialize MyActorSystem
             MyActorSystem = ActorSystem.Create("MyActorSystem");
 
-            // set up props (split props onto own line so easier to read)
+            // set up actors, using props (split props onto own line so easier to read)
             Props consoleWriterProps = Props.Create(typeof (ConsoleWriterActor));
+            IActorRef consoleWriterActor = MyActorSystem.ActorOf(consoleWriterProps, "consoleWriterActor");
+
             Props validationActorProps = Props.Create(() => new ValidationActor(consoleWriterActor));
+            IActorRef validationActor = MyActorSystem.ActorOf(validationActorProps, "validationActor");
+            
             Props consoleReaderProps = Props.Create<ConsoleReaderActor>(validationActor);
-                        
+            IActorRef consoleReaderActor = MyActorSystem.ActorOf(consoleReaderProps, "consoleReaderActor");
+            
             // tell console reader to begin
             consoleReaderActor.Tell(ConsoleReaderActor.StartCommand);
 
