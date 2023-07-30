28e88abf44abd90925b8198f3464de498f68282f
Teddy Alcabelard <talcabelard@d-edge.com>
2023-07-30 17:16:47 +0200

    Add FileObserver

---
+++ src/Unit-1/DoThis/FileObserver.cs
@@ -0,0 +1,82 @@
+using System;
+using System.IO;
+using Akka.Actor;
+
+namespace WinTail
+{
+    /// <summary>
+    /// Turns <see cref="FileSystemWatcher"/> events about a specific file into
+    /// messages for <see cref="TailActor"/>.
+    /// </summary>
+    class FileObserver : IDisposable
+    {
+        private readonly IActorRef tailActor;
+        private readonly string absoluteFilePath;
+        private readonly string fileDir;
+        private readonly string fileNameOnly;
+        private FileSystemWatcher watcher;
+
+        public FileObserver(IActorRef tailActor, string absoluteFilePath)
+        {
+            this.tailActor = tailActor;
+            this.absoluteFilePath = absoluteFilePath;
+            this.fileDir = Path.GetDirectoryName(this.absoluteFilePath);
+            this.fileNameOnly = Path.GetFileName(absoluteFilePath);
+        }
+
+        /// <summary>
+        /// Begin monitoring file.
+        /// </summary>
+        public void Start()
+        {
+
+            // make watcher to obverse our specific file
+            this.watcher = new FileSystemWatcher(fileDir, fileNameOnly);
+
+            // watch our file for changes to the file name,
+            // or new messages being written to file
+            watcher.NotifyFilter = NotifyFilters.FileName | NotifyFilters.LastWrite;
+
+            // assign callbacks for event types
+            watcher.Changed += OnFileChanged;
+            watcher.Error += OnFileError;
+
+            // start watching
+            watcher.EnableRaisingEvents = true;
+        }
+
+        /// <summary>
+        /// Stop monitoring file.
+        /// </summary>
+        public void Dispose()
+        {
+            watcher.Dispose();
+        }
+
+        /// <summary>
+        /// Callback for <see cref="FileSystemWatcher"/> file error events.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void OnFileError(object sender, ErrorEventArgs e)
+        {
+            tailActor.Tell(new TailActor.FileError(fileNameOnly, e.GetException().Message));
+        }
+
+        /// <summary>
+        /// Callback for <see cref="FileSystemWatcher"/> file change events.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void OnFileChanged(object sender, FileSystemEventArgs e)
+        {
+            if (e.ChangeType == WatcherChangeTypes.Changed)
+            {
+                // here we use a special ActorRefs.NoSender
+                // since this event can happen many times,
+                // this is a little microoptimization
+                tailActor.Tell(new TailActor.FileWrite(e.Name), ActorRefs.NoSender);
+            }
+        }
+    }
+}
